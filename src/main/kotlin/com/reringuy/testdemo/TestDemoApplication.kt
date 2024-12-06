7cf3d20db868b4f8bb24661634dbe90da83822b9
Reringuy <jounheringer@outlook.com>
2024-12-05 21:58:00 -0400

    Initial commit

---
+++ src/main/kotlin/com/reringuy/testdemo/TestDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.reringuy.testdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/reringuy/testdemo/TestDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reringuy.testdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
